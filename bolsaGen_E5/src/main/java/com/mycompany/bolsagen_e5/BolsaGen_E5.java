3f0a42efcbe5bcf2936eb548b97afb1195f712ae
C27PP <cjcmendivil@gmail.com>
2025-09-17 08:17:54 -0700

    1 hola

---
+++ bolsaGen_E5/src/main/java/com/mycompany/bolsagen_e5/BolsaGen_E5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bolsagen_e5;
+
+/**
+ *
+ * @author USER
+ */
+public class BolsaGen_E5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
