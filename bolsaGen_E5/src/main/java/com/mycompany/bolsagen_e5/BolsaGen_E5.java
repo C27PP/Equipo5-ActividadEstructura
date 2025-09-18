b833457bee1645ebeadbeea4acb1b183c290d4d4
JuanPabloYepizLeon <juanpabloyepizleon@gmail.com>
2025-09-17 20:26:12 -0700

    YUPIS

--- bolsaGen_E5/src/main/java/com/mycompany/bolsagen_e5/BolsaGen_E5.java
+++ bolsaGen_E5/src/main/java/com/mycompany/bolsagen_e5/BolsaGen_E5.java
@@ -13,5 +13,6 @@ public class BolsaGen_E5 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("HOLA SOY LUIS");
+        System.out.println("YUPIS");
     }
 }
